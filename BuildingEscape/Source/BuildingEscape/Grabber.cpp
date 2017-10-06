bac32734aedb23316e65e372034a17c98494f9d4
Bo black pc <bdbros@gmail.com>
2017-10-06 01:47:59 -0600

    Grabber can grab object now

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -2,7 +2,11 @@
 
 #include "Grabber.h"
 #include "Camera/PlayerCameraManager.h"
+#include "Components/InputComponent.h"
+#include "Components/ActorComponent.h"
 #include "Engine/World.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "Components/PrimitiveComponent.h"
 #include "DrawDebugHelpers.h"
 
 #define OUT
@@ -24,23 +28,32 @@ void UGrabber::BeginPlay()
 
 	UE_LOG(LogTemp, Warning, TEXT("Grabber is here"));
 	// ...
+	FindPhysicsComponent();
+
+	SetupInputComponent();
 	
 }
 
-
-// Called every frame
-void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UGrabber::SetupInputComponent()
 {
-	//--------
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-	//--------
-	// ...
+	InputComponent = GetOwner()->FindComponentByClass <UInputComponent>();
+	//---------
+	if (InputComponent)
+	{
+		InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::GrabObject);
+		InputComponent->BindAction("Grab", IE_Released, this, &UGrabber::ReleaseObject);
+	}
+}
+
+const FHitResult UGrabber::GetFirstPhysicsBodyInReach()
+{
+
 	//Get the player view point this thick
 	FVector PlayerViewPointLocation;
 	FRotator PlayerViewRotation;
 	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
-	OUT PlayerViewPointLocation,
-	OUT PlayerViewRotation
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewRotation
 	);
 
 	//UE_LOG(LogTemp,Warning,TEXT(" location %s , rotation %s"), *PlayerViewPointLocation.ToString(), *PlayerViewRotation.ToString())
@@ -56,7 +69,93 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		0,
 		12.f);
 	//--------
-	// See what we have hit
+	//setup params query
+	FCollisionQueryParams TracePar(FName(TEXT("")), false, GetOwner());
 	//--------
+	// See what we have hit
+	FHitResult Hit;
+
+	GetWorld()->LineTraceSingleByObjectType(OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TracePar
+	);
+
+	if (Hit.Actor != nullptr)
+	{
+		FString _name = Hit.Actor->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("Object got hit: %s"), *_name);
+	}
+	//--------
+	return Hit;
+}
+
+
+void UGrabber::FindPhysicsComponent()
+{
+	/// Look for physicc handler
+	PhysicsHandle = GetOwner()->FindComponentByClass  <UPhysicsHandleComponent>();
+
+	//---------
+	if (PhysicsHandle)
+	{
+		//physics handle is found
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("The name of object missing handle componnet"), *GetOwner()->GetName());
+	}
+}
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	//--------
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	//--------
+	// ...
+	if (PhysicsHandle->GrabbedComponent)
+	{
+		//Get the player view point this thick
+		FVector PlayerViewPointLocation;
+		FRotator PlayerViewRotation;
+		GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+			OUT PlayerViewPointLocation,
+			OUT PlayerViewRotation
+		);
+
+		FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewRotation.Vector() *  60.f;
+
+		PhysicsHandle->SetTargetLocation(LineTraceEnd);
+	}
+
+}
+
+void UGrabber::GrabObject()
+{
+	//Line trace here
+	auto HitRes = GetFirstPhysicsBodyInReach();
+	auto ComponentToGrab = HitRes.GetComponent();
+	auto ActorHit = HitRes.GetActor();
+
+	UE_LOG(LogTemp,Warning,TEXT("Pressed"));
+
+	if (ActorHit)
+	{
+		PhysicsHandle->GrabComponent(ComponentToGrab,NAME_None,ComponentToGrab->GetOwner()->GetActorLocation(),true);
+	}
+
+
+}
+
+void UGrabber::ReleaseObject()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Released"));
+
+	if (PhysicsHandle->GrabbedComponent)
+	{
+		PhysicsHandle->ReleaseComponent();
+	}
 }
 
--- BuildingEscape/Source/BuildingEscape/Grabber.h
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Components/InputComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
 #include "Grabber.generated.h"
 
 
@@ -20,10 +22,24 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+
+
+	
+
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+private:
+	
+	UPhysicsHandleComponent*  PhysicsHandle = nullptr;
+	UInputComponent* InputComponent = nullptr;
 		
+	void GrabObject();
+	void ReleaseObject();
+	void FindPhysicsComponent();
+	void SetupInputComponent();
+
+	const FHitResult GetFirstPhysicsBodyInReach();
 	
 };
